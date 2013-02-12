301cdb484118e618b56d2a5b07cd6585ef075f38
Johannes Schlüter <johannes.schlueter@oracle.com>
2013-02-12 15:50:40 +0100

    Remove declarations

--- php_mysqlnd_memcache.h
+++ php_mysqlnd_memcache.h
@@ -16,25 +16,11 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id$ */
-
 #ifndef PHP_MYSQLND_MEMCACHE_H
 #define PHP_MYSQLND_MEMCACHE_H
 
 extern zend_module_entry mysqlnd_memcache_module_entry;
 #define phpext_mysqlnd_memcache_ptr &mysqlnd_memcache_module_entry
-
-#ifdef PHP_WIN32
-#	define PHP_MYSQLND_MEMCACHE_API __declspec(dllexport)
-#elif defined(__GNUC__) && __GNUC__ >= 4
-#	define PHP_MYSQLND_MEMCACHE_API __attribute__ ((visibility("default")))
-#else
-#	define PHP_MYSQLND_MEMCACHE_API
-#endif
-
-#ifdef ZTS
-#include "TSRM.h"
-#endif
 
 #endif	/* PHP_MYSQLND_MEMCACHE_H */
 
